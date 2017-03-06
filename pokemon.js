920d8aadb881603e6162f151e8b4137740f02558
bmeduho <bmeduho@users.noreply.github.com>
2017-03-06 15:22:09 -0500

    Create pokemon.js

---
+++ pokemon.js
@@ -0,0 +1,18 @@
+var pokemon = {
+	quilava: {
+		default: "http://www.pkparaiso.com/imagenes/xy/sprites/animados/quilava.gif"
+	},
+	nidoran: {
+		default: "male",
+		male: "http://www.pkparaiso.com/imagenes/xy/sprites/animados/nidoran_m.gif",
+		female: "http://www.pkparaiso.com/imagenes/xy/sprites/animados/nidoran_f.gif"
+	},
+	deoxys: {
+		default: "http://www.pkparaiso.com/imagenes/xy/sprites/animados/deoxys.gif",
+		attack: "http://www.pkparaiso.com/imagenes/xy/sprites/animados/deoxys-attack.gif"
+	},
+	altaria: {
+		default: "http://www.pkparaiso.com/imagenes/xy/sprites/animados/altaria.gif",
+		mega: "https://www.pkparaiso.com/imagenes/rubi-omega-zafiro-alfa/sprites/animados/altaria-mega.gif"
+	}
+}
